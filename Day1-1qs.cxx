edc6ebd6e1c0e6cbf378b32045d786a1351c2568
swati2810 <70885820+swati2810@users.noreply.github.com>
2020-12-25 14:34:04 +0530

    Day1 assignment submitted

---
+++ Day1-1qs.cxx
@@ -0,0 +1,16 @@
+//big o notation indicates here is 
+//o(log n)
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int i, n, j;
+	{
+		for(i=1;i<n;i*=2)
+		   for(j=1;j<=n;j++)
+		   cout<<"\nHello";
+	}
+     getch();
+}
